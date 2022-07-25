ff54cbed579f193a632fda8097e17e2df33d3b83
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-07-25 23:58:27 +0530

    Create switchStatement.js

---
+++ switchStatement.js
@@ -0,0 +1,35 @@
+//create a switch statement 
+
+let day = prompt("Enter a num of a week to get what's Today")
+ day = Number.parseInt(day);
+switch (day){
+  case 1: console.log(
+  "Monday  working Day")
+    break;
+    case 2: console.log(
+  "Tuesday continue working Day")
+    break;
+case 3: console.log(
+  "Wednesday mid working Day")
+    break;
+
+    case 4: console.log(
+  "Thursday a working Day")
+    break;
+
+    case 5: console.log(
+  "Friday last working Day")
+    break;
+
+  case 6: console.log(
+  "Saturday weekend Day")
+    break;
+
+    case 7: console.log(
+  "Sunday Prayer Day")
+    break;
+
+     default : console.log(
+  "please enter the numbers from 1-7 only")
+    
+}
