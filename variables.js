63e41975baf14fd5046cfd637447fc1188296281
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-07-29 22:17:23 +0530

    variable.js

---
+++ variables.js
@@ -0,0 +1,14 @@
+//declaring varibles
+var a = 67 
+// a contains 67
+
+console.log(a) // shows that a hold's the value of 67;
+
+a = "harry"
+console.log(a) // here we can re-assign with var value changed from 67 to harry
+
+ let 8harry = 7 // Not allowed this will throw  you an error naming a variable name starting a number cannot allowed in javascript
+ 
+ let var = 7 // a name mustnot contain a number as starting and a any symbols other than dollar are not conventional naming practises
+ 
+ // let and const are the newest way of declaring the variables in js; let can be changeable and const cannot be changble
