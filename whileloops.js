32b77e11207a26edf0464797358c0b3209c7c458
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-07-30 22:48:49 +0530

    updated with minor changes

--- whileloops.js
+++ whileloops.js
@@ -1,4 +1,16 @@
 
+// programme on while loop
+
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+
+
 // programme on do while loops
 let n = prompt("Enter the value of n")
 n = Number.parseInt(n)
