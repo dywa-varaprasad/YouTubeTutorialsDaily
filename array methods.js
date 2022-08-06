9d3a8456d2682fceca0f3ef452a782dfa43282cf
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-06 23:05:06 +0530

    array methods

---
+++ array methods.js
@@ -0,0 +1,40 @@
+// Array Methods
+let numbers = [1, 2, 3, 5, 4,7]
+
+
+let num = numbers.toString() // b is now a string 
+
+
+console.log(b, typeof b)
+
+
+
+let c = numbers.join(" and ")
+// it will creates elements into characters of a string by given parameters inside of join function 
+
+
+console.log(c, typeof c)
+
+
+ let r = numbers.pop() 
+// pop returns the popped last element in the list 
+
+// console.log(num, r)
+
+ let r = numbers.push(56) 
+// push returns the new element pushes into an array
+
+
+// console.log(num, r)
+
+ let r = num.shift()
+// Removes an element from the start of the array
+
+
+// console.log(r, num)
+
+let r = numbers.unshift(78)
+
+//Adds an element from the start of the array.
+
+console.log(r, num)
