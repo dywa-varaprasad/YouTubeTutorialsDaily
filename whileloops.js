642f65d6fa89a82dd72bb53200e5650a6e4165f4
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-17 21:31:04 +0530

    updated
    with typos and bugs

--- whileloops.js
+++ whileloops.js
@@ -1,9 +1,14 @@
 
 // programme on while loop
 
+//asking users to enter a value.
 let n = prompt("Enter the value of n")
+
+//what ever comes from prompt will be a string and explicitly change it's data type into number 
 n = Number.parseInt(n)
 
+
+//looping through the value 
 let i = 0;
 while(i<n){
   console.log(i)
@@ -13,8 +18,10 @@ while(i<n){
 
 // programme on do while loops
 let n = prompt("Enter the value of n")
+//convert 
 n = Number.parseInt(n)
 
+//looping it will check once 
 let i = 10;
 do{
   console.log(i)
